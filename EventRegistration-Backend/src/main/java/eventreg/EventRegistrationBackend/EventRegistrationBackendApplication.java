d5bd5c9c3560e8b9fe070ec886637c486c22d951
atreyi <ranjanatreyi@gmail.com>
2022-07-11 20:58:02 +0100

    Initial commit of backend application

---
+++ EventRegistration-Backend/src/main/java/eventreg/EventRegistrationBackend/EventRegistrationBackendApplication.java
@@ -0,0 +1,19 @@
+package eventreg.EventRegistrationBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class EventRegistrationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventRegistrationBackendApplication.class, args);
+	}
+	@RequestMapping("/")
+    public String greeting(){
+    return "Hello world!";
+  }
+
+}
---
+++ EventRegistration-Backend/src/test/java/eventreg/EventRegistrationBackend/EventRegistrationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package eventreg.EventRegistrationBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRegistrationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
