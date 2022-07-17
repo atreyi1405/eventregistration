ab9b2b329bc0bf1fe55d34d36a902cab32f315cd
atreyi <ranjanatreyi@gmail.com>
2022-07-17 10:58:10 +0100

    test imports commented out

--- EventRegistration-Backend/src/test/java/eventreg/EventRegistrationBackend/EventRegistrationBackendApplicationTests.java
+++ EventRegistration-Backend/src/test/java/eventreg/EventRegistrationBackend/EventRegistrationBackendApplicationTests.java
@@ -1,12 +1,12 @@
 package eventreg.EventRegistrationBackend;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class EventRegistrationBackendApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
